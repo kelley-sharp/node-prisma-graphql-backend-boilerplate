7646cb37be73a59e7e83d1c50c75d311fef7bd9d
Kelley Sharp <kelleylsharp@gmail.com>
2023-06-24 14:50:08 -0400

    save on writing server file

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
+import {
+  COOKIE_OPTIONS,
+  IS_PRODUCTION,
+  SESSION_COOKIE_NAME,
+  SESSION_SECRET,
+} from "./config/constants";
+
+const PORT = 8000;
+
+const app = express();
+
+app.use(
+  session({
+    genid: () => uuidv4(),
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    name: SESSION_COOKIE_NAME,
+    unset: "destroy",
+    cookie: COOKIE_OPTIONS,
+    store: new PrismaSessionStore(new PrismaClient() as any, {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
